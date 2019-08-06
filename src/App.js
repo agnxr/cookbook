c5e7a3f9a9f9271887845a82806311439deaa088
Agnieszka Rakowska <ar21@o2.pl>
2019-08-06 09:57:44 +0200

    add husky & lint-staged

--- src/App.js
+++ src/App.js
@@ -2,27 +2,31 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload. Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Corrupti tenetur rem nesciunt nostrum excepturi voluptates! Soluta aut
-        ipsa libero eligendi perspiciatis quod, veritatis quo! Natus quaerat ab quas perspiciatis
-        ea!
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-        alt="rom"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
-);
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Corrupti tenetur rem nesciunt nostrum excepturi voluptates! Soluta aut
+            ipsa libero eligendi perspiciatis quod, veritatis quo! Natus quaerat ab quas
+            perspiciatis ea!
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+            alt="rom"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
 
 export default App;
