0e9151fe519a09527ea335e2c06e4f496b59c994
Agnieszka Rakowska <ar21@o2.pl>
2019-08-08 14:57:52 +0200

    create styles for secondary button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,15 +1,24 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const Button = styled.button`
-  background-color: ${({ secondary }) => (secondary ? '#f2e2ff' : '#e2eeff')};
+  background-color: #f2e2ff;
   width: 220px;
   height: 47px;
   border: none;
   border-radius: 50px;
   font-family: 'Open Sans', sans-serif;
   font-weight: 500;
-  font-size: ${({ secondary }) => (secondary ? '10px' : '16px')};
+  font-size: 16px;
   text-transform: uppercase;
+
+  ${({ secondary }) =>
+    secondary &&
+    css`
+      background-color: '#f2e2ff';
+      width: 105px;
+      height: 30px;
+      font-size: 10px;
+    `}
 `;
 
 export default Button;
