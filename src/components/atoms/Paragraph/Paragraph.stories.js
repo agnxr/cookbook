a0c8f781c56c4a52b237fc2596126fce31a3061e
Agnieszka Rakowska <ar21@o2.pl>
2019-08-20 14:44:51 +0200

    add pharagraph to storybook

---
+++ src/components/atoms/Paragraph/Paragraph.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import Paragraph from './Paragraph';
+
+storiesOf('Paragraph', module).add('Normal', () => (
+  <Paragraph>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque ab sunt eligendi optio et! Nihil
+    enim itaque saepe, cum pariatur, dolorem quo iusto veniam vel temporibus officiis expedita,
+    debitis a.
+  </Paragraph>
+));
--- src/theme/mainTheme.js
+++ src/theme/mainTheme.js
@@ -7,4 +7,12 @@ export const theme = {
   light: 300,
   medium: 400,
   bold: 800,
+  fontSize: {
+    xxs: '1rem',
+    xs: '1.2rem',
+    s: '1.6rem',
+    m: '2.1rem',
+    l: '2.4rem',
+    xl: '4rem',
+  },
 };
