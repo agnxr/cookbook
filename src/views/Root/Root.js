38a5a5041acbff96da7cc3bfbe68216b46616af5
Agnieszka Rakowska <ar21@o2.pl>
2019-09-20 12:30:15 +0200

    update root view (import Main Template, display content in main template)

--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,22 +1,18 @@
 import React from 'react';
-import { ThemeProvider } from 'styled-components';
+
 import Button from 'components/atoms/Button/Button';
 import ButtonIcon from 'components/atoms/ButtonIcon/ButtonIcon';
-import GlobalStyle from 'theme/GlobalStyle';
-import { theme } from 'theme/mainTheme';
+import MainTemplate from 'templates/MainTemplate';
 
 const App = () => (
-  <div>
-    <GlobalStyle />
-    <ThemeProvider theme={theme}>
-      <>
-        <h1>Hello World</h1>
-        <Button width="150px">CLOSE / SAVE</Button>
-        <Button secondary>Remove</Button>
-        <ButtonIcon active />
-      </>
-    </ThemeProvider>
-  </div>
+  <MainTemplate>
+    <>
+      <h1>Hello World</h1>
+      <Button width="150px">CLOSE / SAVE</Button>
+      <Button secondary>Remove</Button>
+      <ButtonIcon active />
+    </>
+  </MainTemplate>
 );
 
 export default App;
